a01926c544cb596bcec15b4f12e653266828f272
Brandon Zhang <brandonzhang@Brandons-MacBook-Air.local>
2023-11-11 09:50:24 -0500

    Fixed env import

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
+import config from 'react-native-config';
 import { RAPID_API_KEY } from '@env';
 import axios from 'axios';
 
-const rapidApiKey = RAPID_API_KEY;
-
-const useFetch = (params, endpoint) => {
+const useFetch = (endpoint, params) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -16,7 +15,7 @@ const useFetch = (params, endpoint) => {
       ...params
     },
     headers: {
-      'X-RapidAPI-Key': rapidApiKey,
+      'X-RapidAPI-Key': RAPID_API_KEY,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     }
   }
@@ -33,6 +32,7 @@ const useFetch = (params, endpoint) => {
       setData(response.data);
     } catch (error) {
       setError(error);
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
@@ -40,3 +40,4 @@ const useFetch = (params, endpoint) => {
 
   return { data, isLoading, error, fetchData };
 }
+export default useFetch;
